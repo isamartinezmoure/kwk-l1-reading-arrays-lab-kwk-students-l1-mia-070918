d4c5c4830f6a7154b00375dce0004487aa90fc96
isamartinezmoure <isamartinezmoure@gmail.com>
2018-07-10 19:58:30 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -11,7 +11,7 @@ def first_student_by_index(arr)
   return arr[0]
 end
 
-
+studentnames []
 
 def fourth_student_by_index
   # Write a solution that returns the first student in the array student_names
